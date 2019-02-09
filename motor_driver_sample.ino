d8af2e7d26fbf66d8be376346d11157133cafce2
Arnold Perez <alperez@mail.usf.edu>
2019-02-09 17:16:10 -0500

    Added functions to change speed of motor drivers

--- motor_driver_sample.ino
+++ motor_driver_sample.ino
@@ -1,48 +1,74 @@
 // Basic sketch for trying out the Adafruit DRV8871 Breakout
 
-#define MOTOR_IN1 9
-#define MOTOR_IN2 10
+#define MOTOR1_IN1 9
+#define MOTOR1_IN2 10
+#define MOTOR2_IN1 11
+#define MOTOR2_IN2 12
 
 void setup() {
   Serial.begin(9600);
 
   Serial.println("DRV8871 test");
   
-  pinMode(MOTOR_IN1, OUTPUT);
-  pinMode(MOTOR_IN2, OUTPUT);
+  pinMode(MOTOR1_IN1, OUTPUT);
+  pinMode(MOTOR1_IN2, OUTPUT);
+  pinMode(MOTOR2_IN1, OUTPUT);
+  pinMode(MOTOR2_IN2, OUTPUT);
+}
+
+
+
+int convertPercent(double input){
+  int output = input * 2.55;
+  return output;
+}
+
+
+void setSpeed(double leftPercent, double rightPercent){ //sets speed of left and right wheel (0-100%)
+if(leftPercent > 100.1){ //edge cases for invalid values
+  leftPercent = 100;
+}
+if(rightPercent > 100.1){
+  rightPercent = 100;
+}
+if(leftPercent < -100.1){
+  leftPercent = -100;
+}
+if(rightPercent > -100.1){
+  rightPercent = -100;
+}
+
+int leftSpeed = convertPercent(leftPercent); //converts percent to pwm values
+int rightSpeed = convertPercent(rightPercent);
+if(leftSpeed > 0){ //checks if forward or backward
+  digitalWrite(MOTOR1_IN1, LOW);
+  analogWrite(MOTOR1_IN2, i);
+}else if (leftSpeed < 0)
+  {
+  digitalWrite(MOTOR1_IN2, LOW);
+  analogWrite(MOTOR1_IN1, i);
+  }
+
+if(rightSpeed > 0){
+  digitalWrite(MOTOR2_IN1, LOW);
+  analogWrite(MOTOR2_IN2, i);
+}else if (rightSpeed < 0)
+  {
+  digitalWrite(MOTOR2_IN2, LOW);
+  analogWrite(MOTOR2_IN1, i);
+  }
+}
+
+
+void stop(){ //sets driver pwm to 0
+  digitalWrite(MOTOR1_IN1, LOW);
+  digitalWrite(MOTOR1_IN2, LOW);
+  digitalWrite(MOTOR2_IN1, LOW);
+  digitalWrite(MOTOR2_IN2, LOW);
 }
 
 void loop() {
-
-  // ramp up forward
-  digitalWrite(MOTOR_IN1, LOW);
-  for (int i=0; i<255; i++) {
-    analogWrite(MOTOR_IN2, i);
-    delay(10);
-  }
-
-  // forward full speed for one second
-  delay(1000);
-  
-  // ramp down forward
-  for (int i=255; i>=0; i--) {
-    analogWrite(MOTOR_IN2, i);
-    delay(10);
-  }
-
-  // ramp up backward
-  digitalWrite(MOTOR_IN2, LOW);
-  for (int i=0; i<255; i++) {
-    analogWrite(MOTOR_IN1, i);
-    delay(10);
-  }
-
-  // backward full speed for one second
-  delay(1000);
-
-  // ramp down backward
-  for (int i=255; i>=0; i--) {
-    analogWrite(MOTOR_IN1, i);
-    delay(10);
-  }
+setSpeed(50,50);
+delay(5000);
+stop();
 }
