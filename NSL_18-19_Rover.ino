e6e5fdc1adbaba93f5da992b2adf5a1421a87e48
Arnold Perez <alperez@mail.usf.edu>
2019-02-15 15:33:13 -0500

    Added temporary functionality for deployment system

--- NSL_18-19_Rover.ino
+++ NSL_18-19_Rover.ino
@@ -5,9 +5,15 @@
 motor_driver motor;
 xbee xb;
 char c;
+#define DEPLOY1 = 12; //temp declaration for deployment
+#define DEPLOY2 = 13;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("NSL 18-19 Test\n");
+
+  pinMode(DEPLOY1, OUTPUT);
+  pinMode(DEPLOY2, OUTPUT);
 }
 
 void menu(char c){ //menu for ease of changing functionality
@@ -29,6 +35,16 @@ void menu(char c){ //menu for ease of changing functionality
         break;
       case ' ':
       motor.stop();
+        c = '0';
+        break;
+      case '1':
+        digitalWrite(DEPLOY1, HIGH);
+        digitalWrite(DEPLOY2, HIGH);
+        c = '0';
+        break;
+      case '2':
+        digitalWrite(DEPLOY1, LOW);
+        digitalWrite(DEPLOY2, LOW);
         c = '0';
         break;
     }
