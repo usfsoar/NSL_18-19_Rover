4a5d50b372ba7498012421786eef8bd55ed0c662
Ian Sanders <iansanders@mail.usf.edu>
2019-02-17 18:41:16 -0500

    Format

--- NSL_18-19_Rover.ino
+++ NSL_18-19_Rover.ino
@@ -5,11 +5,9 @@
 const byte RELAY1 = 4;
 const byte RELAY2 = 5;
 
-
 motor_driver motor;
 xbee xb;
 char c;
-
 
 /**
  * @brief Automatically runs when Arduino initializes.
@@ -24,42 +22,42 @@ void setup() {
 
 /**
  * @brief Menu for ease of changing functionality.
- * 
+ *
  * @param c Character corresponding to input key.
  */
-void menu(char c){
-    switch (c){
-      case 'w':
-        motor.setSpeedPercent(100,100);
-        break;
-      case 'a':
-        motor.setSpeedPercent(0,100);
-        break;
-      case 's':
-        motor.setSpeedPercent(-100,-100);
-        break;
-      case 'd':
-        motor.setSpeedPercent(100,0);
-        break;
-      case ' ':
-        motor.stop();
-        break;
-      case '1':
-        Serial.print("Both High\n");
-        digitalWrite(RELAY_1, HIGH);
-        digitalWrite(RELAY_2, LOW);
-        break;
-      case '2':
-        Serial.print("Both Low\n");
-        digitalWrite(RELAY_1, LOW);
-        digitalWrite(RELAY_2, LOW);
-        break;
-    }
+void menu(char c) {
+  switch (c) {
+    case 'w':
+      motor.setSpeedPercent(100, 100);
+      break;
+    case 'a':
+      motor.setSpeedPercent(0, 100);
+      break;
+    case 's':
+      motor.setSpeedPercent(-100, -100);
+      break;
+    case 'd':
+      motor.setSpeedPercent(100, 0);
+      break;
+    case ' ':
+      motor.stop();
+      break;
+    case '1':
+      Serial.print("Both High\n");
+      digitalWrite(RELAY_1, HIGH);
+      digitalWrite(RELAY_2, LOW);
+      break;
+    case '2':
+      Serial.print("Both Low\n");
+      digitalWrite(RELAY_1, LOW);
+      digitalWrite(RELAY_2, LOW);
+      break;
+  }
 }
 
 /**
  * @brief Automatically loops while Arduino is running.
- * 
+ *
  */
 void loop() {
   menu(xb.readInput());
