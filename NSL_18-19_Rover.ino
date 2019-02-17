a6c4f8a6fb0404e415d7142da46e765054b2c841
Ian Sanders <iansanders@mail.usf.edu>
2019-02-17 18:37:31 -0500

    Add doxygen to main ino file methods

--- NSL_18-19_Rover.ino
+++ NSL_18-19_Rover.ino
@@ -11,6 +11,9 @@ xbee xb;
 char c;
 
 
+/**
+ * @brief Automatically runs when Arduino initializes.
+ */
 void setup() {
   Serial.begin(9600);
   Serial.println("NSL 18-19 Test\n");
@@ -19,7 +22,12 @@ void setup() {
   pinMode(RELAY2, OUTPUT);
 }
 
-void menu(char c){ //menu for ease of changing functionality
+/**
+ * @brief Menu for ease of changing functionality.
+ * 
+ * @param c Character corresponding to input key.
+ */
+void menu(char c){
     switch (c){
       case 'w':
         motor.setSpeedPercent(100,100);
@@ -42,20 +50,24 @@ void menu(char c){ //menu for ease of changing functionality
         break;
       case '1':
         Serial.print("Both High\n");
-        digitalWrite(RELAY1, HIGH);
-        digitalWrite(RELAY2, LOW);
+        digitalWrite(RELAY_1, HIGH);
+        digitalWrite(RELAY_2, LOW);
         c = '0';
         break;
       case '2':
         Serial.print("Both Low\n");
-        digitalWrite(RELAY1, LOW);
-        digitalWrite(RELAY2, LOW);
+        digitalWrite(RELAY_1, LOW);
+        digitalWrite(RELAY_2, LOW);
         c = '0';
         break;
     }
 }
 
-void loop() { //test code for arduino
-menu(xb.readInput());
-delay(10);
+/**
+ * @brief Automatically loops while Arduino is running.
+ * 
+ */
+void loop() {
+  menu(xb.readInput());
+  delay(10);
 }
