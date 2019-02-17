4e97f5110b35691d4fe57b631c830e34bfcc4a21
Ian Sanders <iansanders@mail.usf.edu>
2019-02-17 14:55:49 -0500

    Format entire file and add docs to stop() and constructor

--- motor_driver.cpp
+++ motor_driver.cpp
@@ -17,14 +17,17 @@ const int MAX_PWM = 255;
  * @param percent 
  * @return int 
  */
-int motor_driver::convertPercent(double percent){
-  if(percent > 100){
+int motor_driver::convertPercent(double percent)
+{
+  if (percent > 100)
+  {
     percent = 100;
   }
-  if(percent < -100){
+  if (percent < -100)
+  {
     percent = -100;
   }
-  
+
   return (percent / 100.0 * MAX_PWM);
 }
 
@@ -34,52 +37,56 @@ int motor_driver::convertPercent(double percent){
  * @param leftPercent Perent in range `[-100, 100]` to set the left motor.
  * @param rightPercent Perent in range `[-100, 100]` to set the right motor.
  */
-void motor_driver::setSpeedPercent(double leftPercent, double rightPercent){
+void motor_driver::setSpeedPercent(double leftPercent, double rightPercent)
+{
 
   int leftSpeed = convertPercent(leftPercent); //converts percent to pwm values
   int rightSpeed = convertPercent(rightPercent);
 
-  if(leftSpeed >0){ //For positive values
+  if (leftSpeed > 0)
+  { //For positive values
     digitalWrite(MOTOR1_IN1, LOW);
     analogWrite(MOTOR1_IN2, leftSpeed);
     Serial.print("\nPos Left %: ");
     Serial.print(leftSpeed);
   }
-  if(rightSpeed > 0){
+  if (rightSpeed > 0)
+  {
     digitalWrite(MOTOR2_IN1, LOW);
     analogWrite(MOTOR2_IN2, rightSpeed);
     Serial.print("\nPos Right %: ");
     Serial.print(rightSpeed);
-    }
+  }
 
-  if(leftSpeed < 0){ //For negative values
+  if (leftSpeed < 0)
+  { //For negative values
     digitalWrite(MOTOR1_IN2, LOW);
     Serial.print("\nNeg Left %: ");
     Serial.print(leftSpeed);
     leftSpeed *= -1; //set the negative value back to positve
     analogWrite(MOTOR1_IN1, leftSpeed);
-    
-    }
+  }
 
-  if(rightSpeed < 0){
+  if (rightSpeed < 0)
+  {
     digitalWrite(MOTOR2_IN2, LOW);
     Serial.print("\nNeg Right %: ");
     Serial.print(rightSpeed);
     rightSpeed *= -1;
     analogWrite(MOTOR2_IN1, rightSpeed);
-    }
+  }
 
-    if(rightSpeed == 0){
-      digitalWrite(MOTOR2_IN1, LOW);
-      digitalWrite(MOTOR2_IN2, LOW);
-    }
+  if (rightSpeed == 0)
+  {
+    digitalWrite(MOTOR2_IN1, LOW);
+    digitalWrite(MOTOR2_IN2, LOW);
+  }
 
-    if(leftSpeed == 0){
-      digitalWrite(MOTOR1_IN1, LOW);
-      digitalWrite(MOTOR1_IN2, LOW);
-    }
-
-
+  if (leftSpeed == 0)
+  {
+    digitalWrite(MOTOR1_IN1, LOW);
+    digitalWrite(MOTOR1_IN2, LOW);
+  }
 }
 
 /**
@@ -88,63 +95,78 @@ void motor_driver::setSpeedPercent(double leftPercent, double rightPercent){
  * @param leftPWM Value in range `[-MAX_PWM, MAX_PWM]` to set the left motor.
  * @param rightPWM Value in range `[-MAX_PWM, MAX_PWM]` to set the right motor.
  */
-void motor_driver::setSpeedPWM(int leftPWM, int rightPWM){ //sets speed of left and right wheel via pwm value
+void motor_driver::setSpeedPWM(int leftPWM, int rightPWM)
+{ //sets speed of left and right wheel via pwm value
 
-  if(leftPWM > 0){
+  if (leftPWM > 0)
+  {
     digitalWrite(MOTOR1_IN1, LOW);
     analogWrite(MOTOR1_IN2, leftPWM);
     Serial.print("\nPos Left PWM: ");
     Serial.print(leftPWM);
   }
-  if(rightPWM > 0){
+  if (rightPWM > 0)
+  {
     digitalWrite(MOTOR2_IN1, LOW);
     analogWrite(MOTOR2_IN2, rightPWM);
     Serial.print("\nPos Right PWM: ");
     Serial.print(rightPWM);
-    
-    }
+  }
 
-  if(leftPWM < 0){
+  if (leftPWM < 0)
+  {
     digitalWrite(MOTOR1_IN2, LOW);
     analogWrite(MOTOR1_IN1, leftPWM);
     Serial.print("\nNeg Left PWM: ");
     Serial.print(leftPWM);
-    }
+  }
 
-  if(rightPWM < 0){
+  if (rightPWM < 0)
+  {
     digitalWrite(MOTOR2_IN2, LOW);
     analogWrite(MOTOR2_IN1, rightPWM);
     Serial.print("\nNeg Right PWM: ");
     Serial.print(rightPWM);
-    }
+  }
 
-  if(rightPWM == 0){
+  if (rightPWM == 0)
+  {
     digitalWrite(MOTOR2_IN1, LOW);
     digitalWrite(MOTOR2_IN2, LOW);
   }
 
-  if(leftPWM < 0){
+  if (leftPWM < 0)
+  {
     digitalWrite(MOTOR1_IN1, LOW);
     digitalWrite(MOTOR1_IN2, LOW);
   }
-    
 }
 
-void motor_driver::stop(){ //sets driver pwm to 0
+/**
+ * @brief Stop both motors.
+ */
+void motor_driver::stop()
+{
+  // `LOW` sets driver PWM to 0
   digitalWrite(MOTOR1_IN1, LOW);
   digitalWrite(MOTOR1_IN2, LOW);
   digitalWrite(MOTOR2_IN1, LOW);
   digitalWrite(MOTOR2_IN2, LOW);
-  Serial.print("\nSTOP");
+  Serial.println("STOP");
 }
 
-motor_driver::motor_driver(){ //constructor to set pins at outputs
+/**
+ * @brief Constructor to set pins as outputs.
+ */
+motor_driver::motor_driver()
+{
   pinMode(MOTOR1_IN1, OUTPUT);
   pinMode(MOTOR1_IN2, OUTPUT);
   pinMode(MOTOR2_IN1, OUTPUT);
   pinMode(MOTOR2_IN2, OUTPUT);
 }
 
-motor_driver::~motor_driver(){
-//Empty
+motor_driver::~motor_driver()
+{
+  //Empty
 }
