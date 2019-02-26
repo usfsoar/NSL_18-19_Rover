c752712577725e11fce3290e4fa524e3bed15fd4
Ian Sanders <iansanders@mail.usf.edu>
2019-02-26 11:44:08 -0500

    Add base boilerplate code for imu

---
+++ imu.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file imu.cpp
+ * @brief Methods for interacting with 9-DOF inertial measuring unit.
+ * 
+ * See
+ * https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/overview
+ * for documentation.
+ */
+
+#include "imu.h"
+#include <Arduino.h>
--- imu.h
+++ imu.h
@@ -1,1 +1,8 @@
-i
+#ifndef IMU_h
+#define IMU_h
+
+class imu{
+
+};
+
+#endif /*IMU_h*/
